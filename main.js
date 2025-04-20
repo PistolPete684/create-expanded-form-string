b82fe65fd8807f31664b92c4189cb23a9188b8ba
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2025-04-20 12:25:21 -0300

    adding code wars function option

--- main.js
+++ main.js
@@ -20,3 +20,13 @@ function expandedForm(num) {
   // Return the string once they are joined together
   return expandedNumber.join(" + ");
 }
+
+
+//Code Wars Answers
+// const expandedForm = n => n.toString()
+//                             .split("")
+//                             .reverse()
+//                             .map( (a, i) => a * Math.pow(10, i))
+//                             .filter(a => a > 0)
+//                             .reverse()
+//                             .join(" + ");
